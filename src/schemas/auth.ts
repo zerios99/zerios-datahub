d0d056dec5fda6de4cf5ff9a40aad3e9b88b5555
Alzain <zainyasser31@gmail.com>
2026-01-22 15:49:49 -0800

    fix

--- src/schemas/auth.ts
+++ src/schemas/auth.ts
@@ -1,12 +1,12 @@
 import { z } from 'zod'
 
 export const loginSchema = z.object({
-  email: z.email().min(1, 'Email is required'),
+  email: z.string().email('Invalid email address').min(1, 'Email is required'),
   password: z.string().min(8, 'Password must be at least 8 characters long'),
 })
 
 export const signupSchema = z.object({
   name: z.string().min(1, 'Full Name is required'),
-  email: z.email().min(1, 'Email is required'),
+  email: z.string().email('Invalid email address').min(1, 'Email is required'),
   password: z.string().min(8, 'Password must be at least 8 characters long'),
 })
