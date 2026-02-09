0a4d2570d248ae593b15f93770323dd7b9d6cd9b
alzain <zainyasser31@gmail.com>
2026-02-09 16:05:10 +0200

    Fix the Route

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,8 @@
-import { prisma } from '@/db'
 import { betterAuth } from 'better-auth'
 import { prismaAdapter } from 'better-auth/adapters/prisma'
 import { tanstackStartCookies } from 'better-auth/tanstack-start'
+
+import { prisma } from '@/db'
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
